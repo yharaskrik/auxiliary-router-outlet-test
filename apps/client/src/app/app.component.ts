6bfc7cda64a1c07c22b12d49bdd25cf646d0c8dc
jaybell <jaycooperbell@gmail.com>
2021-03-24 14:28:14 -0700

    initial commit

---
+++ apps/client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'primary-outlet-aux-route-test-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'client';
+}
